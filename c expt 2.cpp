ab6b8f77be79f20ed21291e1b8414e5c5c11521c
Rafiq1919 <137029189+Rafiq1919@users.noreply.github.com>
2023-06-19 10:41:07 +0530

    Add files via upload

---
+++ c expt 2.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+int start = 2;
+int end = 20;
+printf ("The series of whole numbers excluding zero with a decimal");
+
+
+	
+for (int i = start; i <=end; i += 2) {
+		printf("%d", i);
+}
+}
